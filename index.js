b2dc77ebbbc9ee237e15f8c2077760724640c2ef
SACHIN GIRAWALE <girawalesachin2021@gmail.com>
2024-01-29 08:28:03 +0530

    start thapa technical javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log();
+// console.log('hello world');
+
+
+
+// varibales & values in javascript
+
+// var name = 'John';
+// console.log(name);
+
+var myage = 25;
+
+console.log(myage); 
